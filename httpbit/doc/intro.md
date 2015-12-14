0b41cef80eb05aed57f3cfb3a2b863b8a446855a
MpaK <mrak69@gmail.com>
2015-12-14 13:06:24 +0700

    httpbit

---
+++ httpbit/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to httpbit
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ httpbit/src/httpbit/core.clj
@@ -0,0 +1,20 @@
+(ns httpbit.core
+    (:require [clj-http.client :as client]
+              [clojure.data.json :as json]))
+
+(defn extractJson
+  [responseBody]
+  (get (json/read-str responseBody) "last"))
+
+(defn getCexCurrency
+  [url]
+   (:body
+    (client/get url)))
+
+(defn outCurrency
+  [url]
+   (println
+    (extractJson
+     (getCexCurrency url))))
+
+(defn -main [] (outCurrency "https://cex.io/api/ticker/BTC/USD"))
